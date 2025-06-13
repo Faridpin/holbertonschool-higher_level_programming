3797bb8a3a059723861a02664706a799d4232e1d
root <farid.mahmudlu.std@bhos.edu.az>
2025-06-13 16:14:03 +0000

    23

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -3,8 +3,9 @@ def pow(a, b):
     c = 1
     if b>0:
         for i in range (0, b):
-            c*=a
+            c *= a
     else:
         for i in range (0, abs(b)):
             c /= a
     print(c)
+pow(2, 2)
