1c08feacf4dc2532482bc8f9ef510ad736839a07
root <farid.mahmudlu.std@bhos.edu.az>
2025-06-13 16:30:31 +0000

    29

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
-        return (number % 10)
+        print (number % 10)
     else:
-        return (-(abs(number) % 10))
+        print (-(abs(number) % 10))
+print_last_digit(98)
