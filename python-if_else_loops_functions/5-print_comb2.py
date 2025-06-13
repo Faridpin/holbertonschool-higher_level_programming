fecee2fa1d61c3d32bc834e238c4e51e7b543bcb
root <Faridpin>
2025-06-13 19:43:15 +0000

    37

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range (0,10):
-    print("{}".format('\n'.join(f"{i:.2f}")))
+#!/usr/bin/python3
+for i in range(100):
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
