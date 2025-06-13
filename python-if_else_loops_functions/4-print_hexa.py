fc43b9090e64a2d9efeadd21354efb5b25428493
root <farid.mahmudlu.std@bhos.edu.az>
2025-06-13 09:36:30 +0000

    18

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
 print("{}".format('\n'.join(f"{i} = 0x{i:x}" for i in range(99))))
-
-#for i in range (0,98):
-#    print(i," = 0x", i//16 if i !=  0 else print(i), end="")
-#    if i%16<10:
-#        print(i%10)
-#    else:
-#        print(chr(i%10+97))
