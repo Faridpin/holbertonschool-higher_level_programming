fea67dcdc5c8378f3778e1af11d7877fe96e5f45
root <Faridpin>
2025-06-13 20:06:34 +0000

    46

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    c = 1
-    if b > 0:
-        for i in range(b):
-            c *= a
-    else:
-        for i in range(abs(b)):
-            c /= a
-    return c
+    return a ** b
