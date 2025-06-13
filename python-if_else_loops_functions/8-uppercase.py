c77dd28839cbd25c0666119da09ddc08f8f7cf35
root <farid.mahmudlu.std@bhos.edu.az>
2025-06-13 16:44:22 +0000

    37

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     result = ""
     for i in (str):
-        if "a" <= i <= z:
+        if "a" <= i <= "z":
             result += chr(ord(i) - 32)
         else:
             result += i
