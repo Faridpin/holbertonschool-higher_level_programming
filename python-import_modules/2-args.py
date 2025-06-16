1c189e7331be2cf9e00e5d0f34ffc9d86e8bd544
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-06-16 13:44:14 +0000

    4

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 import sys
 if __name__ == "__main__":
     args = sys.argv[1:]
-    count = 0
+    count = len(args)
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
         print(f"{count} arguments:")
+    count = 0
     for arg in (args):
-        count+=1
+        count += 1
         print(f"{count}: {arg}")
