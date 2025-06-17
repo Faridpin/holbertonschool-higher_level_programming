c1e1bf1f15c96199f810912f0574cb150e994cbe
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-06-17 14:41:11 +0000

    17

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    a = min(my_list)
-    for i in my_list:
-        if a < i:
-            a = i
-    return a
+    if len(my_list) == 0:
+        return None
+    else:
+        a = min(my_list)
+        for i in my_list:
+            if a < i:
+                a = i
+        return a
