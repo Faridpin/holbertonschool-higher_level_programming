8bd3d2b9b3884bdb1039e7217a51731278d54515
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-06-17 14:34:25 +0000

    14

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tup = (len(sentence), sentence[0])
+    if len(sentence) > 0:
+        tup = (len(sentence), sentence[0])
+    else:
+        tup = (len(sentence), None)
     return tup
     ##length = len(sentence)
     ##first = sentence[0]
