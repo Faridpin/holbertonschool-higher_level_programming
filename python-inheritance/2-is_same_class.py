924608aba8bba47d8bf74a9af7a4857b069bb022
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-06-27 11:04:11 +0000

    2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' This file checks if the object is the instance of class '''
+
+
+def is_same_class(obj, a_class):
+    ''' method '''
+    if type(obj) is a_class:
+        return True
+    return False
