500105f92ccdf80361f7098afa05224922ced296
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-06-28 15:13:38 +0000

    10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-''' Class '''
+''' json hard '''
 
 
 class Student:
-    '''Student class'''
+    ''' student class '''
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if isinstance(attrs, list) and all(isinstance(item, str) for item in attrs):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
-        return self.__dict__.copy()
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if isinstance(attr, str) and hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+        else:
+            return self.__dict__
