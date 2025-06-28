6e77124387dbba8ffd533a03c3031fe954af2484
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-06-28 14:34:40 +0000

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,6 +11,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
+
 def main():
     try:
         my_list = load_from_json_file(filename)
@@ -18,6 +19,7 @@ def main():
         my_list = []
     my_list.extend(sys.argv[1:])
     save_to_json_file(my_list, filename)
+
 
 if __name__ == "__main__":
     main()
