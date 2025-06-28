d81a725f69d5ade4196403b01e04ac07f9b77b27
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-06-28 16:14:03 +0000

    12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,13 +6,13 @@ def pascal_triangle(n):
     ''' function '''
     if n <= 0:
         return []
-    pas = [[] for _ in range(n+1)]
+    pas = [[] for _ in range(n)]
     row2 = []
     for i in range(1, n+1):
         for j in range(i):
             if j == 0 or j == len(row2) :
-                pas[i].append(1)
+                pas[i-1].append(1)
             else:
-                pas[i].append(row2[j]+row2[j-1])
-        row2 = pas[i]
+                pas[i-1].append(row2[j]+row2[j-1])
+        row2 = pas[i-1]
     return pas
