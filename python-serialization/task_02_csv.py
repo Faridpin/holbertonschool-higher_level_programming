c17122e09afa56a1768d6180854c5a6fa131b5a2
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-07-01 11:13:47 +0000

    2

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-import csv 
+import csv
 import json
+''' 2 '''
 
 
-def convert_csv_to_json(filename):
+def convert_csv_to_json(csv_filename):
+    ''' 2 '''
     try:
-        with open(filename, "r", encoding="utf-8") as f:
-            with open('data.json', "w", encoding="utf-8") as j:
-                json.dump(list(csv.DictReader(f)), j)
-        return true
+        with open(csv_filename, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            data = list(reader)
+
+        with open('data.json', 'w', encoding='utf-8') as jsonfile:
+            json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+
+        return True
+    except FileNotFoundError:
+        return False
     except Exception:
-        return None
+        return False
