6a710d4a1430669e1e2ccba84e302efe079566a5
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-07-01 10:35:12 +0000

    0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     ''' seriallization '''
 
-    with open(filename, "wb") as f:
-        return pickle.dump(data, f)
+    with open(filename, "w", encoding="utf-8") as f:
+        return json.dump(data, f)
 
 
 def load_and_deserialize(filename):
-    with open(filename, "rb") as f:
-        return pickle.load(f)
+    with open(filename, "rb", encoding="utf-8") as f:
+        return json.load(f)
