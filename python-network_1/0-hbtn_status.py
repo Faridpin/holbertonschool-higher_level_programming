fac25121da12afd8c26cab3b78e027b34f5be45d
root <Faridpin>
2025-07-12 10:33:59 +0000

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-''' open a website '''
-
-
-import urllib.request
-
-
-def fetch_status(url = "https://intranet.hbtn.io/status"):
-    ''' function '''
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+""" Open a website and display content"""
+from urllib import request
 
 if __name__ == "__main__":
-    fetch_status()
+    """ main """
+    url = "https://intranet.hbtn.io/status"
+    req = request.Request(url)
+    req.add_header('cfclearance', 'true')
+
+    with request.urlopen(req) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", utf8_content)
