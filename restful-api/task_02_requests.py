b1cff698409e5e263a1dc2dee2b6497c8c0efebc
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-07-16 11:39:49 +0000

    2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+''' JSON '''
+import requests
+import csv
+
+URL = "https://jsonplaceholder.typicode.com/posts"
+
+def fetch_and_print_posts():
+    ''' function 1 '''
+    response = requests.get(URL)
+    print(requests.get(URL))
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+def fetch_and_save_posts():
+    ''' function 2 '''
+    response = requests.get(URL)
+    if response.status_code == 200:
+        posts = response.json()
+        post_data = [
+                {'id': post['id'], 'title': post['title'], 'body': post['body']}
+                for post in posts
+                ]
+        with open('posts.csv', mode = 'w', newline = '', encoding = 'utf-8') as f:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(post_data)
